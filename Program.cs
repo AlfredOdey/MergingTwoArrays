72213e82e8ec9e698936a842cf3668f4d8bd848e
Alfred Odey <alfredmodey@gmail.com>
2024-06-21 23:43:53 +0100

    Question 4

--- Program.cs
+++ Program.cs
@@ -8,8 +8,29 @@ namespace Merging2Arrays
 {
     internal class Program
     {
+        /*Question 4: Merging Two Arrays
+        Create two arrays of integers:
+        {1, 2, 3} and {4, 5, 6}.
+        Merge the two arrays into a single array.
+        Print the merged array.
+        */
         static void Main(string[] args)
         {
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = { 4, 5, 6 };
+
+            int[] mergedArray = array1.Concat(array2).ToArray();
+
+            //Using a ForEach Loop	
+            Console.Write("Value of merged array: [ ");
+            foreach (var element in mergedArray)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.Write("]\n");
+
+            //Using String.Join
+            Console.WriteLine($"Value of merged array: [ {String.Join(", ", mergedArray)} ]");
         }
     }
 }
